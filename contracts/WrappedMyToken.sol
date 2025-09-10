d1dac016be53ea1555f903ccce63140b610019f1
gutaosky <1093656744@qq.com>
2025-09-10 23:27:21 +0800

    add NFTPoolLockAndRelease.sol

--- contracts/WrappedMyToken.sol
+++ contracts/WrappedMyToken.sol
@@ -5,5 +5,10 @@ pragma solidity ^0.8.27;
 import { MyToken } from "./MyToken.sol";
 
 contract WrappdMyToken is MyToken {
-    constructor(string memory tokenName,string memory tokenSymbol) MyToken(tokenName,tokenSymbol){}
+    constructor(string memory tokenName,string memory tokenSymbol) 
+    MyToken(tokenName,tokenSymbol){}
+
+    function mintTokenWithSpecificTokenId(address to, uint256 tokenId) public {
+        _safeMint(to, tokenId);
+    }
 }
