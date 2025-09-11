60ead18fba33e977f912efbd31984b0db411a917
gutaosky <1093656744@qq.com>
2025-09-12 00:05:09 +0800

    finish the ccip and LockAndRelease deploy js

---
+++ deploy/0_deploy_ccip_simulator.js
@@ -0,0 +1,16 @@
+const { getNamedAccounts, deployments } = require("hardhat");
+
+module.exports = async({getNamedAccounts, deployments}) =>{
+    const firstAccount = await getNamedAccounts()
+    const {deploy, log} = deployments
+
+    log("deploying CCIP Simulator contract")
+    await deploy("CCIPLocalSimulator",{
+        contract: "CCIPLocalSimulator",
+        from: firstAccount,
+        log:true,
+        args:[]
+    })
+    log("CCIP Simulator contract deployed successfully")
+}
+module.exports.tags = ["test","all"]
---
+++ deploy/2_deploy_pool_lock_and_release.js
@@ -0,0 +1,27 @@
+const { getNamedAccounts, ethers } = require("hardhat");
+
+module.exports = async({getNamedAccounts, deployments}) =>{
+    const firstAccount = await getNamedAccounts()
+    const {deploy, log} = deployments
+    log("NFTPoolLockAndRelease deploying...")
+
+    //address _router ,address _link , address nftAddr
+    const ccipSimulatorDeployment = await deployments.get("CCIPLocalSimulator")
+    const ccipSimulator = await ethers.getContractAt("CCIPLocalSimulator", ccipSimulatorDeployment)
+    const ccipConfig = await ccipSimulator.configutation()
+    const sourceRouter = ccipConfig.sourceRouter_
+    const linkToken =  ccipConfig.linkToken_
+    const nftDeployment = await deployments.get("MyToken")
+    const nftAddr = nftDeployment.address
+
+    await deploy("NFTPoolLockAndRelease",{
+        contract: "NFTPoolLockAndRelease",
+        from: firstAccount,
+        log: true,
+        args:[sourceRouter, linkToken,nftAddr]
+
+    })
+    log("NFTPoolLockAndRelease deployed successfully")
+}
+
+module.exports.tag = ["sourcechain","all"]
