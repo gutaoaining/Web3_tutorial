158b6be104200317b6e0c558e77e4338114f381d
gutaosky <1093656744@qq.com>
2025-09-12 23:11:59 +0800

    finish nft contract test

--- deploy/4_deploy_pool_burn_and_mint.js
+++ deploy/4_deploy_pool_burn_and_mint.js
@@ -24,4 +24,4 @@ module.exports = async({getNamedAccounts, deployments}) =>{
     log("NFTPoolBurnAndMint deployed successfully")
 }
 
-module.exports.tag = ["sourcechain","all"]
+module.exports.tag = ["sourcechain", "all"]
---
+++ test/cross-chai-nft.test.js
@@ -0,0 +1,39 @@
+const { getNamedAccounts, ethers } = require("hardhat")
+const { expect } = require("chai")
+
+let firstAccount
+let ccipSimulator
+let nft
+let nftPoolLockAndRelease
+let wnft
+let nftPoolBurnAndMint
+before(async function() {
+    //prepare: variables: contract ,account
+
+    firstAccount = (await getNamedAccounts()).firstAccount
+    await deployments.fixture(["all"])
+    ccipSimulator = await ethers.getContract("CCIPLocalSimulator",firstAccount)
+    nft = await ethers.getContract("MyToken",firstAccount)
+    // nftPoolLockAndRelease = await ethers.getContract("NFTPoolLockAndRelease",firstAccount)
+    wnft = await ethers.getContract("WrappedMyToken",firstAccount)
+    // nftPoolBurnAndMint = await ethers.getContract("NFTPoolBurnAndMint",firstAccount)
+})
+
+describe("source chain -> dest chain tests", async function() {
+    it("test if user can mint a nft from nft contract successfully", async function() {
+        await nft.safeMint(firstAccount)
+        const onwer = await nft.ownerOf(0)
+        expect(onwer).to.equal(firstAccount)
+    })
+})
+
+
+
+// test if user can lock the nft in the pool and send ccip message on source chain
+
+// test if user can get a wrapped nft in dest chain
+
+//dest chain -> source chain
+//test if user can burn the wnft and send ccip message on dest chain
+
+//test if user have the nft unlocked on source chain
