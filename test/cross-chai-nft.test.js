3681599d92691a9b7616febceb69454b7d3fd129
gutaosky <1093656744@qq.com>
2025-09-13 22:13:17 +0800

    finish the lock test

--- test/cross-chai-nft.test.js
+++ test/cross-chai-nft.test.js
@@ -7,6 +7,8 @@ let nft
 let nftPoolLockAndRelease
 let wnft
 let nftPoolBurnAndMint
+let chainSelector
+
 before(async function() {
     //prepare: variables: contract ,account
 
@@ -16,15 +18,34 @@ before(async function() {
     nft = await ethers.getContract("MyToken",firstAccount)
     nftPoolLockAndRelease = await ethers.getContract("NFTPoolLockAndRelease",firstAccount)
     wnft = await ethers.getContract("WrappedMyToken",firstAccount)
-    // nftPoolBurnAndMint = await ethers.getContract("NFTPoolBurnAndMint",firstAccount)
+    nftPoolBurnAndMint = await ethers.getContract("NFTPoolBurnAndMint",firstAccount)
+    const config = await ccipSimulator.configuration()
+    chainSelector = config.chainSelector_
+
 })
 
 describe("source chain -> dest chain tests", async function() {
     it("test if user can mint a nft from nft contract successfully", async function() {
         await nft.safeMint(firstAccount)
-        const onwer = await nft.ownerOf(0)
-        expect(onwer).to.equal(firstAccount)
+        const owner = await nft.ownerOf(0)
+        expect(owner).to.equal(firstAccount)
     })
+
+    it("test if user can lock the nft in the pool and send ccip message on source chain", async function() {
+        await nft.approve(nftPoolLockAndRelease.target, 0)
+        await ccipSimulator.requestLinkFromFaucet(nftPoolLockAndRelease, ethers.parseEther("10"))
+        await nftPoolLockAndRelease.lockAndSendNFT(0, firstAccount, chainSelector, nftPoolBurnAndMint.target)
+        const owner = await nft.ownerOf(0)
+        console.log("owner : " + owner)
+        console.log("nft : " + nft.target)
+        console.log("ccip : " + ccipSimulator.target)
+        console.log("firstAccount: " + firstAccount)
+        console.log("nftPoolLockAndRelease: " + nftPoolLockAndRelease.target)
+        console.log("nftPoolBurnAndMint: " + nftPoolBurnAndMint.target)
+
+        expect(owner).to.equal(nftPoolLockAndRelease)
+    })
+    
 })
 
 
