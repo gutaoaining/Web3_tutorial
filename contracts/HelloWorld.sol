d2ec8f3b0e735834d9598ffb49d4a3c2af2f3dd9
gutaosky <1093656744@qq.com>
2025-09-19 22:03:20 +0800

    add the first contract

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,33 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+contract HelloWorld{
+
+    string public sayHello;
+
+    address public owner;
+
+    mapping(address => string) public phrases; //key -> value
+
+    mapping(address => bool) public passList;
+
+    constructor(){
+        owner = msg.sender;
+        
+    }
+
+    function setHelloWorldPhrases(string memory newPhrases) public {
+        require(passList[msg.sender],"you are not in the passList");
+        phrases[msg.sender] = newPhrases;
+    }
+
+    function addPassList(address addrToAdd) public onlyOwner(){
+        passList[addrToAdd] = true;
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner,"only owner can operate");
+        _;
+    }
+}
